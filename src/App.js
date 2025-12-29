48e7ecdc9d276080d73dd11549ebac7354c3b946
mukeshr08 <tn65vivasaee@gmail.com>
2025-12-29 23:32:23 +0530

    Routing concepts

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { Route,Routes,Link } from 'react-router-dom';
+import About from './About';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
+import Missing from './Missing';
+import Nav from './Nav';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul> 
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/newpost">NewPost</Link></li>
+          <li><Link to="/postpage">PostPage</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/newpost' element={<NewPost/>}/>
+        <Route path='/postpage' element={<PostPage/>}/>
+      </Routes>
+      
+      {/*<Header/>
+      <Nav/>
+      <Home/>
+      <About/>
+      <NewPost/>
+      <PostPage/>
+      <Missing/>
+      <Footer/>*/}
+      
+    </div>
+  );
+}
+
+export default App;
